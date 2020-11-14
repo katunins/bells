3fda5beb33e0d38d00292428b2c2d86d9226b0d6
Pavel Katunin <katunin.pavel@gmail.com>
2020-11-14 15:53:45 +0300

    заполнил 1 блок

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,32 @@
+<div class="header grey-back">
+    <div class="logo col-2">
+        <img src="images/logo.svg" alt="">
+    </div>
+    <div class="menu-block col-5">
+        <?php 
+            $activeRoute = Route::current()->uri;
+            $routes = [
+                '/'=>'Главная', 
+                '/bells'=>'Колокола',
+                '/about'=>'Литейная',
+                '/contacts'=>'Контакты',
+            ];
+        ?>
+
+        @foreach ($routes as $href=>$name)
+        <a @if ($href==$activeRoute) class="active" @endif href="{{ $href }}">{{ $name }}</a>
+        @endforeach
+
+    </div>
+    <div class="phone-block col-2">
+        <img src="images/phone.svg" alt="">
+        <a href="">+7 900 800-22-05</a>
+    </div>
+    <div class="basket-block col-1">
+        <img src="images/basket.svg" alt="">
+        <span id="header-basket-summ">1 262</span>р
+        <button id="basket-arrow">
+            <img src="images/basket-arrow.svg" alt="">
+        </button>
+    </div>
+</div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+    .js('resources/js/app.js', 'public/js')
+    .styles(['resources/css/app.css'], 'public/css/app.css')
+    .styles(['resources/css/header.css'], 'public/css/header.css')
+    .styles(['resources/css/welcome.css'], 'public/css/welcome.css');
