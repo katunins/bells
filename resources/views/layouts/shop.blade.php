3bf3e0fbc74fbfb8d3abff0d68f78c4fe239fe6f
katunins <katunin.pavel@gmail.com>
2020-12-22 15:13:49 +0300

    Пилю модуль детализации

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -8,109 +8,121 @@
     <div class="main-shop-block">
         <div class="filter-block col-2">
             @foreach ($filter as $filterGroupName => $filterGroup)
-                @if (count($filterGroup['data']) > 0)
-                    <div class="filter-group" theme={{ $filterGroupName }}>
-                        <p>{{ $filterGroup['name'] }}</p>
-                        @php
-                        $quantity = 0;
-                        @endphp
-                        @foreach ($filterGroup['data'] as $key => $item)
-                            @php
-                            $quantity++;
-                            @endphp
-                            <li>
-                                <input index={{ $key }} id="{{ $filterGroupName . '_' . $key }}" type="checkbox"
-                                    theme={{ $filterGroupName }} value="{{ $item }}" onchange="changeFilter()"
-                                    name='checkFilter'>
-                                <label @if ($quantity > $maxFilterList) class=" hide"
-                                    needToHide=true
+            @if (count($filterGroup['data']) > 0)
+            <div class="filter-group" theme={{ $filterGroupName }}>
+                <p>{{ $filterGroup['name'] }}</p>
+                @php
+                $quantity = 0;
+                @endphp
+                @foreach ($filterGroup['data'] as $key => $item)
+                @php
+                $quantity++;
+                @endphp
+                <li>
+                    <input index={{ $key }} id="{{ $filterGroupName . '_' . $key }}" type="checkbox"
+                        theme={{ $filterGroupName }} value="{{ $item }}" onchange="changeFilter()" name='checkFilter'>
+                    <label @if ($quantity> $maxFilterList) class=" hide"
+                        needToHide=true
                         @endif
                         for="{{ $filterGroupName . '_' . $key }}">{{ $item }}</label>
-                        </li>
+                </li>
                 @endforeach
 
                 @if ($quantity > $maxFilterList)
-                    <li><button status=false theme={{ $filterGroupName }} class="openmore"
-                            onclick="openMore()">...</button>
-                    </li>
+                <li><button status=false theme={{ $filterGroupName }} class="openmore" onclick="openMore()">...</button>
+                </li>
                 @endif
+            </div>
+            @endif
+            @endforeach
+            <div class="filter-group">
+                <button class="clear-filter" onclick="clearFilter()">Сбросить фильтр</button>
+            </div>
         </div>
-        @endif
-        @endforeach
-        <div class="filter-group">
-            <button class="clear-filter" onclick="clearFilter()">Сбросить фильтр</button>
-        </div>
-    </div>
 
-    <div class="goods-block col-8"></div>
+        <div class="goods-block col-8"></div>
 
-
-</div>
-<div class="footer-shop-block">
-    <div class="pagination-block">
 
     </div>
-    <input class="shop-search" type="text" id="bottom-search" placeholder="Подарок на юбилей ..." oninput="newSearch()">
-</div>
-<div class="modal-detail">
-    <div class="content">
-        <div class="flex-block">
-            <div class="detail">
-                <div class="block">
-                    <h1>Колокол</h1>
-                    <p class="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde esse eum
-                        inventore
-                        saepe sunt. Voluptatum amet, odit praesentium voluptatem blanditiis est in accusantium
-                        similique,
-                        ullam impedit molestias exercitationem numquam minima?</p>
-                </div>
-                <div class="block">
-                    <h3>Выберете подставку</h3>
-                    <div class="stand">
-                        @foreach ([
-                            [
-                                'title' => 'Без подставки',
-                                'image' => '',
-                            ],
-                            [
-                                'title' => 'Дуб',
-                                'image' => 'images/stand/oak.jpg',
-                            ],
-                            [
-                                'title' => 'Орех',
-                                'image' => 'images/stand/oak.jpg',
-                            ],
-                        ] as $key => $item)
-                            <input type="radio" name="desk" id="desk-{{ $key }}" @if ($key == 0) checked
-                        @endif >
-                        <label for="desk-{{ $key }}" style="background-image: url({{ $item['image'] }})">
-                            <span>{{ $item['title'] }}</span>
-                        </label>
-                        @endforeach
+    <div class="footer-shop-block">
+        <div class="pagination-block">
+
+        </div>
+        <input class="shop-search" type="text" id="bottom-search" placeholder="Подарок на юбилей ..."
+            oninput="newSearch()">
+    </div>
+    <div class="modal-detail disable">
+
+        <div class="content">
+
+            <button class="close" onclick="turnDetail()"></button>
+            <div class="flex-block">
+
+                <div class="image-block">
+                    <div class="arrows">
+                        <button id="detail-bells-left" class="transparent-button" onclick="shiftDetailProducts(-1)">
+                            <img style="transform: rotate(180deg);" src="images/arrow-right.svg" alt="">
+                        </button>
+                        <button id="detail-bells-right" class="transparent-button" onclick="shiftDetailProducts(1)">
+                            <img src="images/arrow-right.svg" alt="">
+                        </button>
+                    </div>
+                    <div class="image" style="background-image: url(images/test-bell.jpg)"></div>
+                    <div id="detail-gallery">
+                        {{-- @foreach (['', '', '', '', ''] as $key => $item)
+                        <button @if ($key==0) class="active" @endif
+                            style="background-image: url(images/test-bell.jpg)"></button>
+                        @endforeach --}}
                     </div>
                 </div>
-            </div>
-            <div class="image-block">
-                <div class="arrows">
-                    <button id="detail-bells-left" class="transparent-button" onclick="shiftDetailProducts(-1)">
-                        <img style="transform: rotate(180deg);" src="images/arrow-right.svg" alt="">
-                    </button>
-                    <button id="detail-bells-right" class="transparent-button" onclick="shiftDetailProducts(1)">
-                        <img src="images/arrow-right.svg" alt="">
-                    </button>
-                </div>
-                <div class="image" style="background-image: url(images/test-bell.jpg)"></div>
-                <div class="gallery">
-                    @foreach (['', '', '', '', ''] as $key => $item)
-                        <button @if ($key == 0) class="active"
-                    @endif style="background-image: url(images/test-bell.jpg)"></button>
-                    @endforeach
-                </div>
-            </div>
-        </div>
 
+                <div class="detail">
+                    <div class="block">
+                        <h1 id="detail-title"></h1>
+                        <p id="detail-description"></p>
+                    </div>
+                    <p id="detail-weight" class="weight"></p>
+                    <div id="detail-tags">
+                    </div>
+                    <div class="block">
+                        <h3>Выберите подставку</h3>
+                        <div class="stand">
+                            @foreach ([
+                            [
+                            'title' => 'Без подставки',
+                            'image' => '',
+                            ],
+                            [
+                            'title' => 'Дуб',
+                            'image' => 'images/stand/oak.jpg',
+                            ],
+                            [
+                            'title' => 'Орех',
+                            'image' => 'images/stand/oak.jpg',
+                            ],
+                            ]
+                            as $key => $item)
+                            <div class="desk-group">
+                                <input type="radio" name="desk" id="desk-{{ $key }}" @if ($key==0) checked @endif>
+                                <label for="desk-{{ $key }}" style="background-image: url({{ $item['image'] }})">
+                                    {{-- <span>{{ $item['title'] }}</span>
+                                    --}}
+                                </label>
+                                <p>{{ $item['title'] }}</p>
+                            </div>
+
+                            @endforeach
+                        </div>
+                    </div>
+                    <form class="block to-order">
+                        <div id="to-order-price"></div>
+                        <button>Заказать</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
     </div>
-</div>
 </div>
 
 <script src="js/wharehouse.js"></script>
@@ -169,6 +181,11 @@
             let descriptionBlock = document.createElement('div')
             descriptionBlock.className = 'description-block'
 
+            let inputData = document.createElement('input')
+            inputData.type = 'hidden'
+            inputData.className = 'input-data'
+            inputData.value = JSON.stringify(item)
+
             let toOpen = document.createElement('span')
             toOpen.className = 'arrow-to-open'
             toOpen.innerHTML = '→'
@@ -182,12 +199,13 @@
 
             productCardElem.appendChild(backImgElem)
             productCardElem.appendChild(descriptionBlock)
-
-
+            productCardElem.appendChild(inputData)
 
             // if (item.quantity == 0) productCardElem.classList.add ('none')
 
             goodsBlock.appendChild(productCardElem)
+            productCardElem.onclick = turnDetail
+
         })
     }
 
@@ -199,6 +217,43 @@
         inputElems.forEach(el => {
             if (el.value != currentValue) el.value = currentValue
         })
+    }
+
+    function turnDetail() {
+
+        let detailElem = document.querySelector('.modal-detail')
+        if (detailElem.classList.contains('disable')) {
+            // created_at: "2020-12-17T08:41:44.000000Z"
+            // description: "Этот колокол я взял для теста. Тестовый колокол лучше нигде не применять, так как он не настоящий и сделан только ради того, что бы подстроить алгоритмы и верстку"
+            // filter: {themes: Array(2), mission: Array(2)}
+            // id: 1
+            // images: (4) ["/storage/productImages/1/4ixcf5wF7hBvPQBzrUpxoNNPpXAvQu9diHAJ8k7o.jpeg", "/storage/productImages/1/dww99pb1FmDPLgfwZbeKsz0JhdDpvxBlgp1ytzwl.jpeg", "/storage/productImages/1/P1z8C6ypZ0ksoxm0kDwrl0AWmMCsLEHsJd41h1tg.jpeg", "/storage/productImages/1/AkLuoHfoRmPhHHJRhkLaSjyrO5j83ZAq3YPQHfxv.jpeg"]
+            // price: 5990
+            // quantity: 0
+            // title: "Колокол"
+            // updated_at: "2020-12-21T10:09:01.000000Z"
+            // weight: 0.3
+            let data = JSON.parse(event.currentTarget.querySelector('.input-data').value)
+            document.getElementById('detail-title').innerHTML = data.title
+            document.getElementById('detail-description').innerHTML = data.description
+            document.getElementById('detail-weight').innerHTML = data.weight < 1 ? data.weight * 1000 + ' г' : data
+                .weight + ' кг'
+            let tagsHTML = ''
+            data.filter.themes.forEach(item => {
+                tagsHTML += '<li>' + item + '</li>'
+            })
+            data.filter.mission.forEach(item => {
+                tagsHTML += '<li>' + item + '</li>'
+            })
+            document.getElementById('detail-tags').innerHTML = tagsHTML
+            document.getElementById('to-order-price').innerHTML=data.price
+            let imagesHTML=''
+            data.images.forEach(item=>{
+                imagesHTML+='<button style="background-image: url('+item+')"></button>'
+            })
+            document.getElementById('detail-gallery').innerHTML=imagesHTML
+            detailElem.classList.remove('disable')
+        } else detailElem.classList.add('disable');
     }
 
 </script>
